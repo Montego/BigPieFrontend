1a420a4a4ef073fd001f3a3ff75418835161d213
Montego <bar_d91@mail.ru>
2019-12-04 13:05:15 +0300

    reinit 2.0.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
 	<div id="app">
-		<div id="nav">
-			<router-link to="/login">Login</router-link>
-			<router-link to="/register">Register</router-link>
-		</div>
-		<router-view />
+		<router-view></router-view>
 	</div>
 </template>
 
@@ -15,10 +11,8 @@
 		-moz-osx-font-smoothing: grayscale
 		text-align: center
 		color: #2c3e50
-		y
 	#nav
 		padding: 30px
-
 		a
 			display: block
 			margin-bottom: 30px
@@ -28,3 +22,7 @@
 			&.router-link-exact-active
 				color: #42b983
 </style>
+<script>
+export default {
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../components/Home/Home.vue';
-import Login from '../views/Login/Login';
-import Register from '../views/Register/Register.vue';
+import Welcome from '../pages/Welcome_page.vue';
+import Login from '../pages/Login';
+import Register from '../pages/Register.vue';
+import E404 from '../pages/E404.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
 	{
 		path: '/',
-		name: 'home',
-		component: Home
+		name: 'welcome',
+		component: Welcome
 	},
 	{
 		path: '/login',
@@ -21,11 +22,20 @@ const routes = [
 		path: '/register',
 		name: 'register',
 		component: Register
+	},
+	{
+		path: '',
+		redirect: { name: 'login' }
+	},
+	{
+		path: '*',
+		component: E404
 	}
 ];
 
 const router = new VueRouter({
-	routes
+	routes,
+	mode: 'history'
 });
 
 export default router;
